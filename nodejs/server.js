d0e98f4b7ced104b40590ade9215a38eccfe8f58
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-11-13 11:00:51 +0530

    📝 npm, loadh and nodemon started

--- nodejs/server.js
+++ nodejs/server.js
@@ -22,7 +22,7 @@ const server = http.createServer((req, res) => {
         path += "/about.html";
         res.statusCode = 200;
         break;
-        case "/aboutus":
+      case "/aboutus":
             res.statusCode = 301;
             res.setHeader('Location','/about')
             res.end();
