e83c9f1f2bb77145f75ae9578e7bdaea97f2d50a
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-11-15 18:42:28 +0530

    📝 :404 and .use concept

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ app.get("/aboutus", (req,res)=>{
     res.redirect('/about')
 })
 
-//
+////404 page :  always put 404 page with .use at the bottom most position because it will always give response and if used at the top level
+// then it will always return the response as 404 error
+//We deliberately set the status(404) here because without that it will give status 200 Ok because we're getting response succesfully but we should remember that we're
+//showing error here, which is supposed to be showing status code 404
+app.use((req, res) => {
+    res.status(404).sendFile("./views/404.html", { root: __dirname });
+  });
+  
+
 app.listen(3000, ()=>{
     console.log("server is listening on port 3000");
 });
