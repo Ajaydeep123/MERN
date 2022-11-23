18aad7b56e37cd0d35e9d39be584c69ce00f7e27
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-11-23 18:29:18 +0530

    ✨ bcrypt and email validation

--- foodApp/models/userModel.js
+++ foodApp/models/userModel.js
@@ -56,6 +56,7 @@ userSchema.pre("save", function () {
 });
 
 // here we're using async await because by the time the salt will be brought to our password, we can't allow other things to stop doing their work, so we use async await here.
+// there is only one key per server
 userSchema.pre('save', async function () {
     let salt = await bcrypt.genSalt();
     console.log(salt);
