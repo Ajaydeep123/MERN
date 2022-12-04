af0b13720b99f2fe2a93aad63bff560869eccc53
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-12-04 23:49:23 +0530

    some changes in review router

--- foodApp/Routers/reviewRouter.js
+++ foodApp/Routers/reviewRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const reviewRouter = express.Router();
 const { isAuthorised, protectRoute } = require('../helper');
+const { getAllReviews, top3Review, getPlanReview, createReview, updateReview, deleteReview } = require("../controller/reviewController");
 
 reviewRouter
     .route("/all")
@@ -10,17 +11,18 @@ reviewRouter
     .route("/top3")
     .get(top3Review);
 
+
+reviewRouter.use(protectRoute)
+reviewRouter
+    .route("/crud/:plan")
+    .post(createReview)
+    .patch(updateReview)
+    .delete(deleteReview)
+
+
 //all reviews of a particular plan
 reviewRouter
     .route("/:id")
     .get(getPlanReview);
-
-reviewRouter.use(protectRoute)
-
-reviewRouter
-    .route("/crud:/plan")
-    .post(createReview)
-    .patch(updateReview)
-    .delete(deleteReview)
 
 module.exports = reviewRouter;
